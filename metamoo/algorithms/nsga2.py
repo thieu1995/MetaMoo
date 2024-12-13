f017067a55e6533ba752ab06b1910d903336744c
Thieu Nguyen <nguyenthieu2102@gmail.com>
2024-12-14 00:32:15 +0700

    Add NSGA-II class

---
+++ metamoo/algorithms/nsga2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 15:22, 13/12/2024 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+from metamoo import Optimizer, Population
+
+
+class NSGA2(Optimizer):
+    def __init__(self, epoch, pop_size, selector=None, crossover=None, mutator=None,
+                 repairer=None, seed=None, *args, **kwargs):
+        super().__init__(seed, repairer, *args, **kwargs)
+        self.epoch = epoch
+        self.pop_size = pop_size
+        self.selector = selector
+        self.crossover = crossover
+        self.mutator = mutator
+
+    def evolve(self, epoch):
+
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            # Perform crossover to generate offspring
+            parents = self.generator.choice(self.pop.agents, 2, replace=False)
+            agent = self.crossover.do(parents)
+
+            # Perform mutation on offspring
+            agent = self.mutator.do(agent)
+
+            # Replace old population with offspring (you can add elitism or other strategies here)
+            agent = self.repair_agent(agent)
+            agent = self.evaluate_agent(agent)
+            pop_new.append(agent)
+
+        # Create a pool of agents
+        pop_new.extend(self.pop.agents)
+
+        # Perform non-dominated sorting
+        fronts_idx, _ = self.non_dominated_sorting(pop_new)
+
+        # Select parents for next generation
+        self.pop = Population(self.selector.do(pop_new, fronts_idx, self.pop_size))
