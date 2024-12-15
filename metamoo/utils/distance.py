784a763fc5639aa0e87be4f1d1562810efcb4846
anh9895 <honganh090895@gmail.com>
2024-12-15 16:59:51 +0700

    Update distance module

--- metamoo/utils/distance.py
+++ metamoo/utils/distance.py
@@ -7,8 +7,16 @@
 import numpy as np
 
 
-# Crowding distance calculation
 def calculate_crowding_distance(list_agents=None):
+    """
+    Calculate crowding distance for each individual in the population.
+
+    Args:
+        list_agents: List of individuals (agents of the population).
+
+    Returns:
+       crowding_distance: Numpy array containing the crowding distance for each individual.
+    """
     pop_objs = np.array([agent.objectives for agent in list_agents])
     n_agents, n_objs = pop_objs.shape       # (n_agents, n_objs)
     dist = np.zeros(n_agents)
@@ -18,6 +26,7 @@ def calculate_crowding_distance(list_agents=None):
         dist[idx_sorted[0]] = np.inf
         dist[idx_sorted[-1]] = np.inf
         space = pop_objs[idx_sorted[-1], m] - pop_objs[idx_sorted[0], m]
+        space = 1.0 if space == 0 else space
         for idx in range(1, n_agents - 1):
             dist[idx_sorted[idx]] += (pop_objs[idx_sorted[idx + 1], m] - pop_objs[idx_sorted[idx - 1], m]) / space
     return dist
